cd1b3cec7acf05c5b36ccf3f8da80563a16f836d
Gabriel Verbois <gabriel.verbois@gmail.com>
2024-05-29 16:20:16 +0200

    Test kringing

--- gpr.py
+++ gpr.py
@@ -26,40 +26,74 @@ def extract_dates(file_paths=glob.glob("D:/Cours bioingé/BIR M2/Mémoire/Data/D
 
     return dates
 
+import_data(field_a_paths)
 
-def plot_mean_and_median(file_paths=glob.glob("D:/Cours bioingé/BIR M2/Mémoire/Data/Drone GPR/Field A/*.txt")):
-    gpr_data_tables = []
-    dates = []
-    import_data(file_paths)
+mean_GPR_data_tables = []
+for gpr_data_table in gpr_data_tables:
+    mean_GPR_data_table = gpr_data_table.iloc[:,2].mean()
+    mean_GPR_data_tables.append(mean_GPR_data_table)
 
-    mean_GPR_data_tables = []
-    for gpr_data_table in gpr_data_tables:
-        mean_GPR_data_table = gpr_data_table.iloc[:,2].mean()
-        mean_GPR_data_tables.append(mean_GPR_data_table)
+median_GPR_data_tables = []
+for gpr_data_table in gpr_data_tables:
+    median_GPR_data_table = gpr_data_table.iloc[:,2].median()
+    median_GPR_data_tables.append(median_GPR_data_table)
 
-    median_GPR_data_tables = []
-    for gpr_data_table in gpr_data_tables:
-        median_GPR_data_table = gpr_data_table.iloc[:,2].median()
-        median_GPR_data_tables.append(median_GPR_data_table)
+dates = extract_dates(field_a_paths)
 
-    dates = extract_dates(file_paths)
+# Convert dates to datetime objects
+dates = pd.to_datetime(dates, format="%d/%m/%Y")
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(dates, median_GPR_data_tables, marker='o', label='Median')
-    plt.plot(dates, mean_GPR_data_tables, marker='o', label='Mean')
-    plt.xlabel('Date')
-    plt.ylabel('VWC [/]')
-    plt.title('Evolution of Median and Mean Volumetric Water Content on field A')
-    plt.xticks(dates, rotation=45)
-    plt.gca().xaxis.set_major_locator(plt.MaxNLocator(12))
-    plt.grid(True)
-    plt.legend()
-    plt.show()
-
-    # Reset
-    gpr_data_tables = []
-    dates = []
+plt.figure(figsize=(10, 6))
+plt.plot(dates, median_GPR_data_tables, marker='o', label='Median')
+plt.plot(dates, mean_GPR_data_tables, marker='o', label='Mean')
+plt.xlabel('Date')
+plt.ylabel('VWC [/]')
+plt.title('Evolution of Median and Mean Volumetric Water Content on field A')
+plt.xticks(rotation=45)
+plt.gca().xaxis.set_major_locator(plt.MaxNLocator(12))
+plt.ylim(0.2, 0.5)
+plt.grid(True)
+plt.legend()
+plt.show()
 
 
-plot_mean_and_median()
+# Reset
+gpr_data_tables = []
+dates = []
 
+
+import_data(field_b_paths)
+
+mean_GPR_data_tables = []
+for gpr_data_table in gpr_data_tables:
+    mean_GPR_data_table = gpr_data_table.iloc[:,2].mean()
+    mean_GPR_data_tables.append(mean_GPR_data_table)
+
+median_GPR_data_tables = []
+for gpr_data_table in gpr_data_tables:
+    median_GPR_data_table = gpr_data_table.iloc[:,2].median()
+    median_GPR_data_tables.append(median_GPR_data_table)
+
+dates = extract_dates(field_b_paths)
+
+# Convert dates to datetime objects
+dates = pd.to_datetime(dates, format="%d/%m/%Y")
+
+plt.figure(figsize=(10, 6))
+plt.plot(dates, median_GPR_data_tables, marker='o', label='Median')
+plt.plot(dates, mean_GPR_data_tables, marker='o', label='Mean')
+plt.xlabel('Date')
+plt.ylabel('VWC [/]')
+plt.title('Evolution of Median and Mean Volumetric Water Content on field B')
+plt.xticks(rotation=45)
+plt.gca().xaxis.set_major_locator(plt.MaxNLocator(12))
+plt.ylim(0.2, 0.5)
+plt.grid(True)
+plt.legend()
+plt.show()
+
+# Reset
+gpr_data_tables = []
+dates = []
+
+    
