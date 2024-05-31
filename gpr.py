32e8087c40eb16305a58bde9808670d166cc9eb8
Django184 <gabriel.verbois@gmail.com>
2024-05-31 14:09:28 +0200

    huhu

--- gpr.py
+++ gpr.py
@@ -3,9 +3,19 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import skgstat as skg
+from skgstat import models
+import gstatsim as gs
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from sklearn.preprocessing import QuantileTransformer 
+import pyproj # for reprojection
 
 field_a_paths = glob.glob("D:/Cours bioingé/BIR M2/Mémoire/Data/Drone GPR/Field A/*.txt") # return all file paths that match a specific pattern
 field_b_paths = glob.glob("D:/Cours bioingé/BIR M2/Mémoire/Data/Drone GPR/Field B/*.txt")
+
+# Define the transformer for WGS84 to UTM (UTM zone 32N)
+transformer = pyproj.Transformer.from_crs("epsg:4326", "epsg:32632", always_xy=True)
+
 
 gpr_data_tables = []
 def import_data(file_paths=glob.glob("D:/Cours bioingé/BIR M2/Mémoire/Data/Drone GPR/Field A/*.txt")):
@@ -26,91 +36,23 @@ def extract_dates(file_paths=glob.glob("D:/Cours bioingé/BIR M2/Mémoire/Data/D
 
     return dates
 
-import_data(field_a_paths)
+extract_dates(field_a_paths)
 
-mean_GPR_data_tables = []
-for gpr_data_table in gpr_data_tables:
-    mean_GPR_data_table = gpr_data_table.iloc[:,2].mean()
-    mean_GPR_data_tables.append(mean_GPR_data_table)
+# Read csv file
+field_a_example = pd.read_csv(field_a_paths[10], sep = "  ", engine="python")
 
-median_GPR_data_tables = []
-for gpr_data_table in gpr_data_tables:
-    median_GPR_data_table = gpr_data_table.iloc[:,2].median()
-    median_GPR_data_tables.append(median_GPR_data_table)
+# Convert latitude and longitude to UTM coordinates
+utm_x, utm_y = transformer.transform(field_a_example.iloc[:,1].values, field_a_example.iloc[:,0].values)
 
-dates = extract_dates(field_a_paths)
-
-# Convert dates to datetime objects
-dates = pd.to_datetime(dates, format="%d/%m/%Y")
-
+# Plot the sampling points
 plt.figure(figsize=(10, 6))
-plt.plot(dates, median_GPR_data_tables, marker='o', label='Median')
-plt.plot(dates, mean_GPR_data_tables, marker='o', label='Mean')
-plt.xlabel('Date')
-plt.ylabel('VWC [/]')
-plt.title('Evolution of Median and Mean Volumetric Water Content on field A')
-plt.xticks(rotation=45)
-plt.gca().xaxis.set_major_locator(plt.MaxNLocator(12))
-plt.ylim(0.2, 0.5)
-plt.grid(True)
-plt.legend()
-plt.show()
-
-
-# Reset
-gpr_data_tables = []
-dates = []
-
-
-import_data(field_b_paths)
-
-mean_GPR_data_tables = []
-for gpr_data_table in gpr_data_tables:
-    mean_GPR_data_table = gpr_data_table.iloc[:,2].mean()
-    mean_GPR_data_tables.append(mean_GPR_data_table)
-
-median_GPR_data_tables = []
-for gpr_data_table in gpr_data_tables:
-    median_GPR_data_table = gpr_data_table.iloc[:,2].median()
-    median_GPR_data_tables.append(median_GPR_data_table)
-
-dates = extract_dates(field_b_paths)
-
-# Convert dates to datetime objects
-dates = pd.to_datetime(dates, format="%d/%m/%Y")
-
-plt.figure(figsize=(10, 6))
-plt.plot(dates, median_GPR_data_tables, marker='o', label='Median')
-plt.plot(dates, mean_GPR_data_tables, marker='o', label='Mean')
-plt.xlabel('Date')
-plt.ylabel('VWC [/]')
-plt.title('Evolution of Median and Mean Volumetric Water Content on field B')
-plt.xticks(rotation=45)
-plt.gca().xaxis.set_major_locator(plt.MaxNLocator(12))
-plt.ylim(0.2, 0.5)
-plt.grid(True)
-plt.legend()
-plt.show()
-
-# Reset
-gpr_data_tables = []
-dates = []
-
-
-# Plot sampling points of first file
-first_file_data_table = pd.read_csv(field_a_paths[0], sep = "  ", engine="python")
-plt.figure(figsize=(10,6))
-scatter = plt.scatter(first_file_data_table.iloc[:,1], first_file_data_table.iloc[:,0], c=first_file_data_table.iloc[:,2], cmap='viridis', label='Sampling points')
+scatter = plt.scatter(utm_x, utm_y, c=field_a_example.iloc[:,2], cmap='viridis', label='Sampling points')
 plt.xlabel('X [m]')
 plt.ylabel('Y [m]')
-plt.title('Sampling points of first file')
-plt.xticks(rotation=45)
+plt.title(f'Field A GPR sampling {dates[10]}')
 cb = plt.colorbar(scatter)
 cb.set_label('Volumetric Water Content [/]')
-plt.grid(True)
+plt.grid(False)
 plt.legend()
 plt.show()
-
-
-
 
